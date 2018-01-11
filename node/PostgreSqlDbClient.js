c45b509d1f8cc58504dd910e62623d023b427a0b
Krisztián Kurucz <kukrisz@gmail.com>
2018-01-11 01:08:26 +0100

    [feature] create node domain and the Postgre Client which runs on the NodeJS server

---
+++ node/PostgreSqlDbClient.js
@@ -0,0 +1,174 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 2, maxerr: 50, node: true */
+/*global */
+(function () {
+  "use strict";
+
+  var objectAssign = require('object-assign');
+  var ConnectionPool = require('pg-pool');
+  var types = require('pg').types;
+  var BIT_OID = 1560;
+
+  /**
+   * @private
+   * @type {DomainManager}
+   * The DomainManager passed in at init.
+   */
+  var _domainManager = null;
+
+  /**
+   * @private
+   * @type {ConnectionPool}
+   * ConnectionPool.
+   */
+  var _pool = null;
+
+  /**
+   * @private
+   * @type {Object}
+   * ConnectionPool configurations.
+   */
+  var _poolConfig = {
+    min: 5,
+    max: 10
+  };
+
+  function _cmdExecStmnt(config, requestId, sqlStr, inputParams) {
+    if (!_pool) {
+      console.log('Pool will be created');
+      _pool = new ConnectionPool(objectAssign(config, _poolConfig));
+      console.log('Pool is created');
+      _pool.on('error', error);
+      // _pool.on('connect', connect);
+      // _pool.on('acquire', acquire);
+      types.setTypeParser(BIT_OID, function (val) {
+        return parseInt(val);
+      })
+    }
+
+    exec(_pool, requestId, sqlStr, inputParams);
+  }
+
+  function _cmdClose() {
+    if (_pool) {
+      _pool.end();
+      _pool = null;
+    }
+  }
+
+  function exec(connection, requestId, sql, inputs) {
+    sql = sql.toString();
+
+    // var request = new Request(sql, function (err, rowCount, rows) {
+    //     if (err) {
+    //       console.error('Statement failed: ', err);
+    //       return;
+    //     }
+    //
+    //     connection.release();
+    //     _domainManager.emitEvent("postgreSqlDbClient", "statementComplete", [requestId, rowCount, rows]);
+    //   }
+    // );
+    // request.on('columnMetadata', columnMetadata);
+    // request.on('row', function (columns) {
+    //   _domainManager.emitEvent("postgreSqlDbClient", "rowReceived", [requestId, columns]);
+    // });
+    // request.on('done', requestDone);
+    //
+    // inputs.forEach(function (input) {
+    //   request.addParameter(input.name, input.type, input.value);
+    // });
+
+    console.log('Execute query', sql, inputs);
+    connection.query(sql, inputs, function (err, res) {
+      if (err) {
+        console.error(err);
+        error(err);
+      }
+
+      _domainManager.emitEvent("postgreSqlDbClient", "statementComplete", [requestId, res.rowCount, res.rows]);
+    });
+  }
+
+  function requestDone(rowCount, more) {
+    //console.log(rowCount + ' rows');
+  }
+
+  function error(err, client) {
+    console.error('Connection fails to connect to the SQL Server', err);
+    _cmdClose();
+    _domainManager.emitEvent("postgreSqlDbClient", "error", err);
+  }
+
+  function columnMetadata(columnsMetadata) {
+    // columnsMetadata.forEach(function (column) {
+    //   console.log(column);
+    // });
+  }
+
+  /**
+   * Initialize the 'postgreSqlDbClient' domain with commands and events.
+   * @param {DomainManager} domainManager The DomainManager for the server
+   */
+  function init(domainManager) {
+    _domainManager = domainManager;
+    if (!domainManager.hasDomain("postgreSqlDbClient")) {
+      domainManager.registerDomain("postgreSqlDbClient", {major: 0, minor: 1});
+    }
+
+    domainManager.registerCommand(
+        "postgreSqlDbClient",       // domain name
+        "execStmnt",    // command name
+        _cmdExecStmnt,   // command handler function
+        false,          // this command is synchronous in Node
+        "Execute SQL statement",
+        [ // parameters
+          {name: "config", type: "object", description: "configurations to connect to the database"},
+          {name: "requestId", type: "string", description: "request identification"},
+          {name: "sql", type: "string", description: "given SQL statement will be executed"},
+          {name: "input", type: "array", description: "input parameters if it SQL statement is parameterized"}
+        ],
+        [ // return value
+          // { name: "resultSet", type: "object", description: "result of the given SQL statement" }
+        ]
+    );
+
+    domainManager.registerCommand(
+        "postgreSqlDbClient",       // domain name
+        "close",    // command name
+        _cmdClose,   // command handler function
+        false,          // this command is synchronous in Node
+        "Close all connections",
+        [],
+        []
+    );
+
+    domainManager.registerEvent(
+        "postgreSqlDbClient",           // domain name
+        "statementComplete",  // event name
+        [ // event arguments
+          {name: "requestId", type: "string", description: "request identification"},
+          {name: "rowCount", type: "string", description: "Number of rows"},
+          {name: "rows", type: "array", description: "Result of the SQL statement"}
+        ]
+    );
+
+    domainManager.registerEvent(
+        "postgreSqlDbClient",           // domain name
+        "rowReceived",  // event name
+        [ // event arguments
+          {name: "requestId", type: "string", description: "request identification"},
+          {name: "row", type: "object", description: "Collection of a row values"}
+        ]
+    );
+
+    domainManager.registerEvent(
+        "postgreSqlDbClient",           // domain name
+        "error",  // event name
+        [ // event arguments
+          {name: "err", type: "object", description: "Error object"},
+        ]
+    );
+  }
+
+  exports.init = init;
+}());

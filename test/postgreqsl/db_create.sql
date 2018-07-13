441f9ba7e953edfcfb2ce4d6222c3cb86027f53f
kkrisz1 <krisz.bme.vik@gmail.com>
2018-07-13 15:17:08 +0200

    [ci] postgres

--- test/postgreqsl/db_create.sql
+++ test/postgreqsl/db_create.sql
@@ -1,5 +1,5 @@
 -- Database: Untitled
 -- Author: 
-CREATE DATABASE "user" WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+CREATE DATABASE "user" WITH ENCODING = 'UTF8' CONNECTION LIMIT = -1;
 CREATE USER "user" WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE "user" to "user";

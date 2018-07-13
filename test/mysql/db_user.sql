e9cc904bf16703d7efd4604c2e989bcc3dd487ad
kkrisz1 <krisz.bme.vik@gmail.com>
2018-07-13 09:24:31 +0200

    [test] user settings

--- test/mysql/db_user.sql
+++ test/mysql/db_user.sql
@@ -1,6 +1,6 @@
---use mysql;
---update user set authentication_string=PASSWORD('password') where user='root';
---update user set plugin='mysql_native_password';
---FLUSH PRIVILEGES;
+-- use mysql;
+-- update user set authentication_string=PASSWORD('password') where user='root';
+-- update user set plugin='mysql_native_password';
+-- FLUSH PRIVILEGES;
 CREATE USER 'user'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON \`user\`.* TO 'user'@'%';

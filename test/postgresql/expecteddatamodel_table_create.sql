efc3fc03cd98ab6b7723a01647341338136569b7
kkrisz1 <19638316+kkrisz1@users.noreply.github.com>
2022-07-26 21:19:45 +0200

    Test data for duplicated columns (#94)
    * SQL query to include schema in first pg_class join"

--- test/postgresql/expecteddatamodel_table_create.sql
+++ test/postgresql/expecteddatamodel_table_create.sql
@@ -24,7 +24,7 @@ CREATE TABLE public.person (
     department_id integer,
     department_sysid integer,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id, department_sysid) REFERENCES department (id, sysid)
+    FOREIGN KEY (department_id, department_sysid) REFERENCES public.department (id, sysid)
 );
 
 COMMENT ON TABLE public.person IS 'Person';
@@ -45,7 +45,7 @@ CREATE TABLE public.dog (
     owner bigint,
     chip_no char(128) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (owner) REFERENCES person (id)
+    FOREIGN KEY (owner) REFERENCES public.person (id)
 );
 
 COMMENT ON TABLE public.dog IS 'Dog';
@@ -53,3 +53,63 @@ COMMENT ON COLUMN public.dog.id IS 'Dog ID';
 COMMENT ON COLUMN public.dog.name IS 'Dog name';
 COMMENT ON COLUMN public.dog.owner IS 'Dog owner';
 COMMENT ON COLUMN public.dog.chip_no IS 'Chip number';
+
+-- Database: user
+-- DROP TABLE other.person CASCADE;
+-- DROP TABLE other.dog CASCADE;
+-- DROP TABLE other.department CASCADE;
+CREATE SCHEMA other;
+comment ON SCHEMA other IS 'standard public schema';
+ALTER SCHEMA other owner TO "user";
+GRANT CREATE, USAGE ON SCHEMA other TO PUBLIC;
+
+CREATE TABLE other.department (
+    id integer NOT NULL,
+    sysid integer NOT NULL,
+    name varchar(128) NOT NULL,
+    PRIMARY KEY (id, sysid)
+);
+
+COMMENT ON TABLE other.department IS 'Department';
+COMMENT ON COLUMN other.department.id IS 'Department ID';
+COMMENT ON COLUMN other.department.sysid IS 'Department system ID';
+COMMENT ON COLUMN other.department.name IS 'Department name';
+
+CREATE TABLE other.person (
+                               id bigint NOT NULL,
+                               name varchar(32) NOT NULL,
+                               age integer NOT NULL,
+                               identity_card_no char(16) NOT NULL,
+                               driver_licence_no char(16),
+                               department_id integer,
+                               department_sysid integer,
+                               PRIMARY KEY (id),
+                               FOREIGN KEY (department_id, department_sysid) REFERENCES other.department (id, sysid)
+);
+
+COMMENT ON TABLE other.person IS 'Person';
+COMMENT ON COLUMN other.person.id IS 'Person ID';
+COMMENT ON COLUMN other.person.name IS 'Person name';
+COMMENT ON COLUMN other.person.age IS 'Person age';
+COMMENT ON COLUMN other.person.identity_card_no IS 'National identity card number';
+COMMENT ON COLUMN other.person.driver_licence_no IS 'Driver license number';
+COMMENT ON COLUMN other.person.department_id IS 'Department ID';
+COMMENT ON COLUMN other.person.department_sysid IS 'Department system ID';
+
+ALTER TABLE other.person
+    ADD UNIQUE (identity_card_no, driver_licence_no);
+
+CREATE TABLE other.dog (
+                            id bigint NOT NULL,
+                            name varchar(32) NOT NULL,
+                            owner bigint,
+                            chip_no char(128) NOT NULL,
+                            PRIMARY KEY (id),
+                            FOREIGN KEY (owner) REFERENCES other.person (id)
+);
+
+COMMENT ON TABLE other.dog IS 'Dog';
+COMMENT ON COLUMN other.dog.id IS 'Dog ID';
+COMMENT ON COLUMN other.dog.name IS 'Dog name';
+COMMENT ON COLUMN other.dog.owner IS 'Dog owner';
+COMMENT ON COLUMN other.dog.chip_no IS 'Chip number';

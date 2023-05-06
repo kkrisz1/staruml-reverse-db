2e8115b08d1c57704aec60dc3a75f436c20ca1a4
kkrisz1 <krisz.bme.vik@gmail.com>
2023-05-06 14:30:44 +0200

    [bugfix] handle pool as a promise

--- postgresql/PostgreSqlDbClient.js
+++ postgresql/PostgreSqlDbClient.js
@@ -1,36 +1,40 @@
 const DbClient = require("../db/DbClient");
 
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 const types = require('pg').types;
 const BIT_OID = 1560;
 
 class PostgreSqlDbClient extends DbClient {
-  constructor(options) {
-    super(options, null, {
-      min: 5,
-      max: 10,
-      log: options.logging ? console.log : null
-    });
-  }
-
-  _cmdExecStmnt(requestId, sqlStr, inputParams) {
-    if (!this.pool) {
-      this.pool = new Pool(Object.assign(this.options, this.poolConfig));
-      types.setTypeParser(BIT_OID, val => parseInt(val));
+    constructor(options) {
+        super(options, null, {
+            min: 5,
+            max: 10,
+            log: options.logging ? console.log : null
+        });
     }
 
-    return this.pool.query(sqlStr.toString(), inputParams);
-  }
+    _cmdExecStmnt(requestId, sqlStr, inputParams) {
+        if (!this.pool) {
+            this.pool = new Pool(Object.assign(this.options, this.poolConfig));
+            types.setTypeParser(BIT_OID, val => parseInt(val));
+        }
 
-  _cmdClose() {
-    return new Promise(resolve => {
-      if (this.pool) {
-        this.pool.end(() => console.log("Pool is ended..."));
-        this.pool = null;
-      }
-      resolve();
-    });
-  }
+        return this.pool.query(sqlStr.toString(), inputParams);
+    }
+
+    _cmdClose() {
+        if (!this.pool) {
+            return new Promise(resolve => {
+                resolve();
+            })
+                .then(() => console.log("No pool is created..."));
+        }
+        return new Promise(resolve => {
+            this.pool.end(() => console.log("Pool is ended..."));
+            this.pool = null;
+            resolve();
+        });
+    }
 }
 
 module.exports = PostgreSqlDbClient;
